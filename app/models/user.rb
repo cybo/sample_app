4faf9317efcfb74dbf888a3158011c9b5cdf5887
cybo <cybo747@gmail.com>
2013-10-26 03:03:45 -0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A\w+[\w+\-.]+\w+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_EMAIL_REGEX = /\A\w+[\w+\-.]+\w+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A([a-z\d]+[\-.]*)+\w+@([a-z\d]+[\-.])*([a-z\d]+)\.[a-z]+\z/
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,8 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com .foo@bar.com -foo@bar.ca]
+                     foo@bar_baz.com foo@bar+baz.com .foo@bar.com -foo@bar.ca,
+                     foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -61,16 +62,15 @@ describe User do
     it { should_not be_valid }
   end
 
-  # describe "when email address is not lowercase" do
-  #   before do
-  #     @user.email.upcase!
-  #     @user.save
-  #   end
-  
-  #   if @user.email == @user.email.lowercase then 
-  #     it { should_not be_valid }
-  #   end
-  # end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@bAr.Com" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 
   describe "when password is not present" do
     before { @user = User.new(name: "The Dude", email: "thedude@abides.net", 
